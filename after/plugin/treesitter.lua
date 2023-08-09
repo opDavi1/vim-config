f288b0b3893f9cbd3a4964a2afc3336d14265783
opDavi1 <121784309+opDavi1@users.noreply.github.com>
2023-08-08 23:29:31 -0700

    initial setup

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,11 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+
+  sync_install = false,
+
+  highlight = {
+    enable = true,
+
+   additional_vim_regex_highlighting = false,
+  },
+}
---
+++ lua/core/packer.lua
@@ -0,0 +1,36 @@
+return require('packer').startup(function(use)
+
+    --packer
+    use 'wbthomason/packer.nvim'
+
+    --catppuccin
+    use { "catppuccin/nvim", as = "catppuccin" }
+
+    --telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    --mason-lspconfig
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+
+    --cmp
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/nvim-cmp'
+
+    --luasnip
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+
+    --treesitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+end)
