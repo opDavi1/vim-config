e965dc8fc5b382f55c2b2eac6d8e0c7cd149bffa
opDavi1 <121784309+opDavi1@users.noreply.github.com>
2023-08-09 23:37:04 -0700

    changed cmp bindings

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,39 +1,42 @@
---require("mason").setup {
---    log_level = vim.log.levels.DEBUG
---}
---
+
 require('mason').setup {}
 require("mason-lspconfig").setup {
-	ensure_installed = { "lua_ls", "tsserver", "pyright", "html", "cssls", "jdtls" }
-}
+    ensure_installed = { "lua_ls",
+    "tsserver",
+    "pyright",
+    "html",
+    "cssls",
+    "jdtls",
+}}
 
 local cmp = require'cmp'
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
-  cmp.setup({
+cmp.setup({
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      end,
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
     },
     window = {
-       completion = cmp.config.window.bordered(),
-       documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
+
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      --['<C-Space>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
     }),
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' }, -- For luasnip users.
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
     }, {
-      { name = 'buffer' },
+        { name = 'buffer' },
     })
-  })
+})
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").lua_ls.setup {capabilities = capabilities}
